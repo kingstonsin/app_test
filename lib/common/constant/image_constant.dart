067d207e6f192a347334d5a92620aa4b749fb931
Kingston <kingstonsin97@gmail.com>
2024-06-24 17:24:43 +0800

    root screen with navigation bottom bar

---
+++ lib/common/constant/image_constant.dart
@@ -0,0 +1,10 @@
+class ImageConstant {
+  static String imagePath = 'assets/images';
+
+  static String navDashboard = '$imagePath/ic_nav_dashboard.png'; //TODO
+  static String navMusicList = '$imagePath/ic_nav_music_list.png'; //TODO
+
+  static String navFavorate = '$imagePath/ic_nav_favorate.png'; //TODO
+
+  static String navSetting = '$imagePath/ic_nav_setting.png'; //TODO
+}
