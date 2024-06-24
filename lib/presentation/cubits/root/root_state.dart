332586ae6dea715591eef234e839901736268415
Kingston <150878264+kssin2023@users.noreply.github.com>
2024-06-24 16:56:54 +0800

    init go router

---
+++ lib/presentation/cubits/root/root_state.dart
@@ -0,0 +1,21 @@
+enum NavbarItem { dashboard, list, favourite, setting }
+
+class RootState {
+  final NavbarItem navbarItem;
+  final int index;
+
+  const RootState({
+    this.navbarItem = NavbarItem.dashboard,
+    this.index = 0,
+  });
+
+  RootState copyWith({
+    int? index,
+    NavbarItem? navbarItem,
+  }) {
+    return RootState(
+      index: index ?? this.index,
+      navbarItem: navbarItem ?? this.navbarItem,
+    );
+  }
+}
